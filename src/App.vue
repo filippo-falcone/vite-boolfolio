3e68170d8db92d1bc605c4053648786dc4cf34ea
filippo-falcone <falconefilippo98@hotmail.com>
2024-06-10 11:16:48 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <div class="container text-center">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+</style>
