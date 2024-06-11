34299b1f428c813a6d69155bdbac82359bf4e37a
filippo-falcone <falconefilippo98@hotmail.com>
2024-06-11 21:10:33 +0000

    feat: add pagination and activePage function

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -8,19 +8,35 @@ export default {
     },
     data() {
         return {
+            currentPage: 1,
+            prevPageUrl: null,
+            nextPageUrl: null,
+            pages: null,
+            isActive: 0,
             projects: []
         };
     },
     methods: {
-        getProjects() {
-            axios.get('http://192.168.1.155:8000/api/projects')
+        getProjects(pageNumber) {
+            axios.get('http://192.168.1.155:8000/api/projects', {
+                params: {
+                    page: pageNumber
+                }
+            })
                 .then(response => {
-                    this.projects = response.data.results;
+                    this.projects = response.data.results.data;
+                    this.currentPage = response.data.results.current_page;
+                    this.prevPageUrl = response.data.results.prev_page_url;
+                    this.nextPageUrl = response.data.results.next_page_url;
+                    this.pages = response.data.results.last_page;
                 });
+        },
+        activePage(index) {
+            this.isActive = index;
         }
     },
     mounted() {
-        this.getProjects();
+        this.getProjects(this.currentPage);
     }
 }
 </script>
@@ -30,6 +46,25 @@ export default {
         <div class="container">
             <div class="row row-cols-1 row-cols-md-2 g-4">
                 <ProjectCard v-for="project in projects" :projectInfo="project" :key="project.id"></ProjectCard>
+            </div>
+            <div class="my-3">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-center">
+                        <li v-if="prevPageUrl" class="page-item">
+                            <a class="page-link"
+                                @click="getProjects(currentPage - 1); activePage(isActive - 1);">Previous</a>
+                        </li>
+                        <li v-for="page, index in pages" class="page-item">
+                            <a class="page-link" :class="{ 'active': index == isActive }"
+                                @click="getProjects(page); activePage(index);">{{ page
+                                }}</a>
+                        </li>
+                        <li v-if="nextPageUrl" class="page-item">
+                            <a class="page-link"
+                                @click="getProjects(currentPage + 1); activePage(isActive + 1);">Next</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </section>
