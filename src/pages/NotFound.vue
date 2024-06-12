41800766bcc0797cd771b5efab88dd2d2b8553ac
filippo-falcone <falconefilippo98@hotmail.com>
2024-06-12 10:24:40 +0000

    feat: add notFound.vue

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    mounted() {
+        console.log(this.$route.path);
+    }
+}
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <div class="d-flex flex-column align-items-center justify-content-center">
+                <h1>404 error</h1>
+                <p>Page not found</p>
+                <div class="d flex">
+                    <router-link :to="{ name: 'home' }" role="button">
+                        Home
+                    </router-link>
+                    or
+                    <router-link :to="{ name: 'projects' }" role="button">
+                        Projects
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss"></style>
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -15,7 +15,11 @@ export default {
         getProject() {
             axios.get(`http://192.168.1.155:8000/api/projects/${this.$route.params.slug}`)
                 .then((response) => {
-                    this.project = response.data.project;
+                    if (response.data.success) {
+                        this.project = response.data.project;
+                    } else {
+                        this.$router.push({ name: 'not-found' });
+                    }
                 });
         }
     },
@@ -29,11 +33,6 @@ export default {
     <section class="py-4">
         <div class="container">
             <ProjectCard v-if="project" :projectInfo="project"></ProjectCard>
-            <div v-else class="d-flex flex-column align-items-center justify-content-center">
-                <h1>Project not found</h1>
-                <div class="mb-2">Return to</div>
-                <router-link class="btn btn-primary" :to="{ name: 'projects' }" role="button">Projects</router-link>
-            </div>
         </div>
     </section>
 </template>
